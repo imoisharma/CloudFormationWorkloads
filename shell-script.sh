f27e6c0d2ecaefa84f86459790bbb93b18704a62
imoisharma@icloud.com <imoisharma@icloud.com>
2021-02-24 23:51:26 +1100

    updated the shell-script

--- shell-script.sh
+++ shell-script.sh
@@ -1,2 +1,5 @@
+# #!/bin/bash
+# "$1 (aws cloudformation create-stack --stack-name myteststack12 --template-body file://test.json --parameters file://parameters.json --profile mohit-ssa)"
+
 #!/bin/bash
-ec2-metadata -i
+echo `aws cloudformation create-stack --stack-name myteststack13 --template-body file://test.json --parameters file://parameters.json --profile mohit-ssa` 
