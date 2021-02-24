51a95ffffd479b372840ec65772a24b727dfa642
imoisharma@icloud.com <imoisharma@icloud.com>
2021-02-25 10:45:31 +1100

    end fi is creating an issue which fixed now

--- deploy.sh
+++ deploy.sh
@@ -12,11 +12,12 @@ if  [ $1 = $cr ]
 then
     # echo `aws ec2 import-key-pair --key-name "my-public-key" --public-key-material fileb://my-public-key.pub --profile mohit-ssa`
     echo "Your CloudFormation Stack CREATE_IN_PROGRESS BEGINS ..."
-    echo `aws cloudformation create-stack --stack-name myteststack1 --template-body file://test.json --parameters file://parameters.json --profile mohit-ssa`
+    echo `aws cloudformation create-stack --stack-name myteststack --template-body file://test.json --parameters file://parameters.json --profile mohit-ssa`
     echo "EC2 instance Resources Creation Initated ..."
     sleep 5
     echo "Resource creation may take up to 1-2 minutes or sometimes even more due to poor network, so please be patient. Running ..."
     sleep 10
+    echo "Installing the Apache Server ..."
     echo "Waiting for your EC2 instance to assigned the Public Ip from Amazon IPV4 pool address ..."
     echo "..."
     sleep 120
@@ -25,19 +26,19 @@ then
     echo `grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' URL > output-url`
     IP=`cat output-url`
     echo `cat output-url |xargs curl -o output -vvvvv`
-    echo "Congratulations! You have successfully launched the Apache Server. To see the server, refer to this: http://$IP (Please note, if the server is still loading, 
-          which means ec2-instance is still initializing, so please be patient or check your ec2-instance status for here 
-          https://console.aws.amazon.com/ec2/home?#Instances:instanceState=running"
-    # echo `output`
+    echo "Congratulations! You have successfully launched the Apache Server. To see the server, refer to this: http://$IP" 
+    echo "(Please note, if the server is still loading the page, 
+          which means ec2-instance is still initializing the resources, so please be patient or check your ec2-instance status for here 
+          https://console.aws.amazon.com/ec2/home?#Instances:instanceState=running)"
 
 elif [ $1 = $del ]
 then
     echo "Your CloudFormation Stack DELETE_IN_PROGRESS ..."
-    echo `aws cloudformation delete-stack --stack-name myteststack407 --profile mohit-ssa`
+    echo `aws cloudformation delete-stack --stack-name myteststack --profile mohit-ssa`
+
 else
     echo "You provided wrong argument. Please pass the (create or delete) argument only. 
     For example- 
-    1. './deploy.sh create' for creating the CloudFormation Stack
-    2. './deploy.sh delete' for deleting the CloudFormation Stack"
-# close the if 
-fi 
+    1. './deploy.sh create' for creating the CloudFormation Stack 
+    2. './deploy.sh delete' for deleting the CloudFormation Stack "
+fi
