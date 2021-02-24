a0a5beed28b4386c850e36eb8a7f813a8d5c0c11
imoisharma@icloud.com <imoisharma@icloud.com>
2021-02-25 02:52:44 +1100

    makes changes to the script

--- deploy.sh
+++ deploy.sh
@@ -11,7 +11,7 @@ del=delete
 if  [ $1 = $cr ]
 then
     echo "Your CloudFormation Stack CREATE_IN_PROGRESS ..."
-    echo `aws cloudformation create-stack --stack-name myteststack47 --template-body file://test.json --parameters file://parameters.json --profile mohit-ssa`
+    echo `aws cloudformation create-stack --stack-name myteststack85 --template-body file://test.json --parameters file://parameters.json --profile mohit-ssa`
     echo "EC2 instance Resource Creation Initated ..."
     # sleep 5
     # echo "this can take up to 60 seconds ..."
@@ -24,12 +24,12 @@ then
 elif [ $1 = $del ]
 then
     echo "Your CloudFormation Stack DELETE_IN_PROGRESS ..."
-    echo `aws cloudformation delete-stack --stack-name myteststack --profile mohit-ssa`
+    echo `aws cloudformation delete-stack --stack-name myteststack407 --profile mohit-ssa`
 else
     echo "You provided wrong argument. Please pass the (create or delete) argument only. 
     For example- 
-    1. ./deploy.sh create for creating the CloudFormation Stack
-    2. ./deploy.sh delete for deleting the CloudFormation Stack"
+    1. './deploy.sh create' for creating the CloudFormation Stack
+    2. './deploy.sh delete' for deleting the CloudFormation Stack"
 # close the if 
 fi 
 
