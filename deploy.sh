4ac3dbe31943815cfdbf439582d04c2a4bc7f72d
imoisharma@icloud.com <imoisharma@icloud.com>
2021-02-25 00:41:13 +1100

    changed the permission

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,4 @@
-# #!/bin/bash
-# "$1 (aws cloudformation create-stack --stack-name myteststack12 --template-body file://test.json --parameters file://parameters.json --profile mohit-ssa)"
-
-#!/bin/bash
-# echo `aws cloudformation create-stack --stack-name myteststack13 --template-body file://test.json --parameters file://parameters.json --profile mohit-ssa` 
+#!/bin/bash 
 cr=create
 del=delete
 #echo $1
@@ -13,7 +9,10 @@ then
 elif [ $1 = $del ]
 then
     echo "Your CloudFormation Stack DELETE_IN_PROGRESS ..."
-    echo `aws s3 ls --profile mohit-ssa`
+    echo `aws cloudformation create-stack --stack-name myteststack13 --template-body file://test.json --parameters file://parameters.json --profile mohit-ssa`
 else
-    echo "You provided wrong argument. Please pass the (create or delete) argument only. For example- sh ./deploy create or sh ./deploy delete"
+    echo "You provided wrong argument. Please pass the (create or delete) argument only. 
+    For example- 
+    1. ./deploy create for creating the CloudFormation Stack
+    2. ./deploy delete for deleting the CloudFormation Stack"
 fi 
